a3c578c2cbc6d7e053bf414f92f9cf6395e3a4e0
GPigoso <guilhermepigoso@gmail.com>
2024-02-17 17:12:31 -0300

    Criada a funcao, "criar_usuario" e "filtrar_usuario"

--- main.py
+++ main.py
@@ -56,8 +56,23 @@ def exibir_extrato(saldo, /, *, extrato):
 
 def criar_usuario(usuarios):
     cpf = input("Informe o CPF (somente os numeros!): ")
+    usuario = filtrar_usuario(cpf, usuarios)
 
+    if usuario:
+        print("\n@@@ Já existe usuário com esse CPF! @@@")
+        return
 
+    nome = input("Informe o nome completo: ")
+    data_nascimento = input("Informe a data de nascimento (dd-mm-aaaa): ")
+    endereco = input("Informe o endereço (logradouro, nro - bairro - cidade/sigla estado): ")
+
+    usuarios.append({"nome": nome, "data_nascimento": data_nascimento, "cpf": cpf, "endereco": endereco})
+
+    print("=== Usuário criado com sucesso! ===")
+
+def filtrar_usuario(cpf, usuarios):
+    usuarios_filtrados = [usuario for usuario in usuarios if usuario["cpf"] == cpf]
+    return usuarios_filtrados[0] if usuarios_filtrados else None
 
 def main():
     LIMITE_SAQUES = 3
@@ -94,6 +109,9 @@ def main():
         elif opcao == "3":
             exibir_extrato(saldo, extrato = extrato)
         
+        elif opcao == "6":
+            criar_usuario(usuarios)
+
         elif opcao == "7":
             print("Saindo do sistema....")
             break
